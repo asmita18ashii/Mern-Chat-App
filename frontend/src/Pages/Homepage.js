ea011ca3319feb2179b60c76baf7b6f54d213253
asmita18ashii <asmita18ashii@gmail.com>
2024-11-27 22:26:49 +0530

    frontend

---
+++ frontend/src/Pages/Homepage.js
@@ -0,0 +1,129 @@
+import {
+  Box,
+  Container,
+  Text,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router";
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
+import './Homepage.css';
+import ResetPass from "../components/Authentication/ResetPassword";
+
+
+function Homepage() {
+  const history = useHistory();
+  const [forgotTab, setForgotTab] = useState(false);
+
+  const handleTabClick = (event, panelId) => {
+    const tabs = document.querySelectorAll('.tab');
+    const panels = document.querySelectorAll('.tab-panel');
+
+    tabs.forEach(tab => tab.classList.remove('active'));
+    panels.forEach(panel => panel.style.display = 'none');
+
+    if (event) {
+      event.currentTarget.classList.add('active');
+    }
+    document.getElementById(panelId).style.display = 'block';
+  };
+
+  const handleForgotPass = (e) => {
+    setForgotTab(true)
+    handleTabClick(e, 'reset-pass-panel')
+  }
+
+  const handleLoginTab = (e) => {
+    setForgotTab(false)
+    handleTabClick(e, 'login-panel')
+  }
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("userInfo"));
+
+    if (user) history.push("/chats");
+  }, [history]);
+
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        display="flex"
+        justifyContent="center"
+        p={3}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderColor="rgb(111, 50, 111)"
+        borderWidth="2px"
+      >
+        <Text fontSize="5xl" fontFamily="fantasy" fontWeight="500" color="rgb(111, 50, 111)" fontStyle="Bold">
+          Talk-A-Tive
+        </Text>
+      </Box>
+      <Box display="flex"
+        justifyContent="center"
+        p={3}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderColor="rgb(111, 50, 111)"
+        borderWidth="2px">
+
+        <div className="tabs">
+          <ul className="tab-list">
+            {
+              !forgotTab &&
+              <>
+                <li
+                  className="tab active"
+                  onClick={(e) => handleLoginTab(e)}
+                >
+                  Login
+                </li>
+                <li
+                  className="tab"
+                  onClick={(e) => handleTabClick(e, 'signup-panel')}
+                >
+                  SignUp
+                </li>
+              </>
+            }
+
+            {
+              forgotTab &&
+              <>
+                <li
+                  className="tab active"
+                >
+                  Reset Password
+                </li>
+                <li
+                  className="tab"
+                  onClick={(e) => handleLoginTab(e)}
+                >
+                  Login
+                </li>
+              </>
+            }
+          </ul>
+          <div id="login-panel"  className="tab-panel">
+            <Login />
+            <small
+              className='forget-txt'
+              onClick={(e) => handleForgotPass(e)}>
+              Forgot Password?
+            </small>
+          </div>
+          <div id="signup-panel" className="tab-panel" style={{ display: 'none' }}>
+            <Signup />
+          </div>
+          <div id="reset-pass-panel" className="tab-panel" style={{ display: 'none' }}>
+            <ResetPass onCompletion={handleLoginTab} />
+          </div>
+        </div>
+      </Box>
+
+    </Container>
+  );
+}
+
+export default Homepage;
---
+++ frontend/src/components/Authentication/ResetPassword.js
@@ -0,0 +1,153 @@
+import React, { useState } from 'react'
+import { Button } from "@chakra-ui/button";
+import { FormControl, FormLabel } from "@chakra-ui/form-control";
+import { Input, InputGroup, InputRightElement } from "@chakra-ui/input";
+import axios from 'axios'
+import { useToast } from "@chakra-ui/react";
+
+
+const ResetPassword = ({ onCompletion }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [show, setShow] = useState(false);
+    const [show2, setShow2] = useState(false);
+    const toast = useToast();
+
+    const [loading, setLoading] = useState(false);
+    const [confirmPassword, setConfirmPass] = useState('');
+
+    const handleClick = () => setShow(!show);
+    const handleClickShow = () => setShow2(!show);
+
+
+    const submitHandler = async () => {
+        setLoading(true);
+        if (!email || !password) {
+            // toast({
+            //   title: 'Please fill all the fields',
+            //   status: 'warning',
+            //   duration: 3000,
+            //   position: 'top-right',
+            //   isClosable: true,
+            // });
+            setLoading(false);
+            return;
+        }
+
+        try {
+            const config = {
+                headers: {
+                    'Content-type': 'application/json'
+                },
+            };
+            const { data } = await axios.put('http://localhost:5000/user/reset-password', { email, password }, config);
+
+            toast({
+              title: 'Login Successful.',
+              status: 'success',
+              duration: 3000,
+              position: 'top-right',
+              isClosable: true,
+            });
+            localStorage.setItem('userInfo', JSON.stringify(data));
+            setLoading(false);
+            onCompletion(); // For example, pass a new event or any required data
+        } catch (err) {
+            toast({
+              title: 'Error Occurred!',
+              description: err?.response?.data.message,
+              status: 'error',
+              duration: 3000,
+              position: 'top-right',
+              isClosable: true,
+            });
+            setLoading(false);
+        }
+    };
+
+    return (
+        <div className="container">
+            <FormControl id="email" isRequired>
+                <FormLabel>Email Address</FormLabel>
+                <Input
+                    value={email}
+                    type="email"
+                    placeholder="Enter Your Email Address"
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </FormControl>
+
+            <FormControl id="password" isRequired>
+                <FormLabel>Password</FormLabel>
+                <InputGroup size="md">
+                    <Input
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        type={show ? "text" : "password"}
+                        placeholder="Enter password"
+                    />
+                    <InputRightElement width="4.5rem">
+                        <Button h="1.75rem" size="sm" onClick={handleClick}>
+                            {show ? "Hide" : "Show"}
+                        </Button>
+                    </InputRightElement>
+                </InputGroup>
+            </FormControl>
+            <FormControl id="password" isRequired>
+                <FormLabel>Confirm Password</FormLabel>
+                <InputGroup size="md">
+                    <Input
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPass(e.target.value)}
+                        type={show ? "text" : "password"}
+                        placeholder="Enter password"
+                    />
+                    <InputRightElement width="4.5rem">
+                        <Button h="1.75rem" size="sm" onClick={handleClickShow}>
+                            {show2 ? 'Hide' : 'Show'}
+                        </Button>
+                    </InputRightElement>
+                </InputGroup>
+            </FormControl>
+
+            {/* <div className="form-control">
+                <label className='label'>Confirm Password</label>
+                <div className="input-group">
+                    <input className="input-box" type={show2 ? 'text' : 'password'} placeholder="Confirm password" onChange={(event) => setConfirmPass(event.target.value)} />
+                    <button className='button-show' onClick={() => setShow2(!show2)}>{show2 ? 'Hide' : 'Show'}</button>
+                </div>
+            </div> */}
+
+            {/* <button
+                className="button-submit"
+                onClick={submitHandler}
+                disabled={loading}
+            >
+                {loading ? 'Loading...' : 'Submit'}
+            </button> */}
+
+            <Button
+                backgroundColor="black"
+                borderColor="rgb(111, 50, 111)"
+                color="rgb(111, 50, 111)"
+                borderWidth="2px"
+                width="100%"
+                style={{ marginTop: 15 }}
+                onClick={submitHandler}
+                isLoading={loading}
+                _hover={{
+                    bg: "rgb(111, 50, 111)", // Background color on hover
+                    color: "black", // Text color on hover
+                }}
+                _active={{
+                    bg: "rgb(111, 50, 111)", // Background color on hover
+                    color: "black", // Text color when active
+                }}
+            >
+                {loading ? 'Loading...' : 'Submit'}
+            </Button>
+        </div>
+    );
+}
+
+export default ResetPassword
